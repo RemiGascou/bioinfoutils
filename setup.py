6b6e3cf104ec44dc2d291f623e56a160fa1e780f
Remi GASCOU <remi@gascou.net>
2019-01-24 12:49:25 +0100

    Added setup and packaging

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name         = 'bioinfoutils',
+    version      = '0.1.1',
+    description  = '',
+    url          = 'http://github.com/Remigascou/bioinfoutils',
+    author       = 'Remi GASCOU',
+    author_email = 'remi.gascou@gmail.com',
+    license      = 'GPL2',
+    packages     = ['bioinfoutils'],
+    zip_safe     = False
+)
